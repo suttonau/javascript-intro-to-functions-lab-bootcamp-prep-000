092473c44bb1103d355abed99b3cc79d264f2b0f
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-07-16 15:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
     return ("I can't hear you!")
-  } else if {
+  } else if
     (string === string.toUpperCase()) {
       return ("YES INDEED!")
     }
-  } else {
+  } else 
     return ("I love you, too.")
   }
 }
