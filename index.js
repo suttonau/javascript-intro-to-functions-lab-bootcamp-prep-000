1c2d153b75b8cfbe3401fc16cd585240ffc6e781
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-07-16 14:53:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = "hello!" 
+
+lowercase.toLowerCase() === lowercase
+
+
+function sayHiToGrandma(string) {
+  if (lowercase) {
+    return "I can't hear you!"
+  } else if (uppercase) {
+    return "YES INDEED!"
+  } else {
+    return "I love you, too."
+  }
+}
